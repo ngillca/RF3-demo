5f0b17c07b942a49d15de3326d615d6cee2a430a
ngillca <sngill.ca@gmail.com>
2022-03-16 11:50:47 -0700

    Adding rotational cube geometry for inital demo

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef } from 'react';
+
+import "./App.scss";
+
+//R3F
+import { Canvas, useFrame } from "@react-three/fiber";
+// Deai - R3F
+
+
+const SpinningBox = () => {
+  const mesh = useRef(null);
+  useFrame(() => (mesh.current.rotation.x = mesh.current.rotation.y += 0.01));
+  
+  return (
+    <mesh ref={mesh}>
+      <boxBufferGeometry attach='geometry' args={[1, 1, 1]}  />
+      <meshStandardMaterial attach='material' />
+    </mesh>
+  )
+}
 
 function App() {
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <>
+  <Canvas>
+    <SpinningBox />
+  </Canvas>
+  </>
   );
 }
 
